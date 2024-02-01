9870c927e7df66bd9a0a799a3433c2a6473efbd4
Elena Dobruk <elena.dobruk@yandex.ru>
2024-02-01 15:58:39 +0300

    fix gendiff/gendiff.py typos

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -3,9 +3,9 @@ import argparse
 
 def parse():
     parser = argparse.ArgumentParser(
-    prog='gendiff',
-    description='Compares two configuration files and shows a difference.',
-)
+        prog='gendiff',
+        description='Compares two configuration files and shows a difference.',
+    )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     return parser.parse_args(['-h'])
