1b2d248a49ccd8798f036f89f47487c8f3c82e0c
Elena Dobruk <elena.dobruk@yandex.ru>
2024-02-02 16:59:51 +0300

    step3: add json parsing and diff

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,16 +1,44 @@
-import argparse
+import json
 
 
-def parse():
-    parser = argparse.ArgumentParser(
-        prog='gendiff',
-        description='Compares two configuration files and shows a difference.',
-    )
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.add_argument(
-        '-f',
-        '--format',
-        help='set format of output'
-    )
-    return parser.parse_args()
+def get_data(file):
+    with open(file) as file:
+        return json.load(file)
+
+
+def to_format(dct):
+    result = '{\n'
+    for k, v in dct.items():
+        if v[0] == ' ':
+            result += f' + {k}: {v[1]}\n'
+        elif v[1] == ' ':
+            result += f' - {k}: {v[0]}\n'
+        elif v[0] == v[1]:
+            result += f'   {k}: {v[0]}\n'
+        else:
+            result += f' - {k}: {v[0]}\n'
+            result += f' + {k}: {v[1]}\n'
+    return result + '}'
+
+
+def generate_diff(file1_path, file2_path):
+    dict1 = get_data(file1_path)
+    dict2 = get_data(file2_path)
+    dict3 = {}
+    for k in sorted(dict1.keys()|dict2.keys()):
+        if k in dict1 and k in dict2:
+            dict3[k] = [dict1[k], dict2[k]]
+        elif k in dict1 and k not in dict2:
+            dict3[k] = [dict1[k], ' ']
+        else:
+            dict3[k] = [' ', dict2[k]]
+    for v in dict3.values():
+        if v[0] is False:
+            v[0] = 'false'
+        if v[0] is True:
+            v[0] = 'true'
+        if v[1] is False:
+            v[1] = 'false'
+        if v[1] is True:
+            v[1] = 'true'
+    return to_format(dict3)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
-from gendiff.gendiff import parse
+from gendiff.parser import parse
+from gendiff.gendiff import generate_diff
 
 
 def main():
-    parse()
+    result = parse()
+    file1 = result.first_file
+    file2 = result.second_file
+    print(generate_diff(file1, file2))
 
 
 if __name__ == '__main__':
