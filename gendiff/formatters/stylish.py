93e5abb8eab70bf4841442b01faff9d2bc0d9b0c
Elena Dobruk <elena.dobruk@yandex.ru>
2024-02-09 13:20:57 +0300

    step6: add formatter "stylish", refactor gendiff.py

---
+++ gendiff/formatters/stylish.py
@@ -0,0 +1,43 @@
+import itertools
+
+INDENT = 4
+
+
+def to_stylish(diff, depth=1):
+    lines = []
+    for item in diff:
+        key = item.get('key')
+        value = item.get('val')
+        status = item.get('status')
+        start_indent = ' ' * (depth * INDENT - 2)
+        end_indent = ' ' * ((depth - 1) * INDENT)
+        if status == 'nested':
+            children = item.get('children')
+            lines.append(f'{start_indent}  {key}: {to_stylish(children, depth + 1)}')
+        elif status == 'deleted':
+            lines.append(f'{start_indent}- {key}: {to_str(value, depth * INDENT)}')
+        elif status == 'added':
+            lines.append(f'{start_indent}+ {key}: {to_str(value, depth * INDENT)}')
+        elif status == 'unchanged':
+            lines.append(f'{start_indent}  {key}: {to_str(value, depth * INDENT)}')
+    result = '\n'.join(lines)
+    return '{\n' + result + f'\n{end_indent}}'
+
+
+def to_str(current_value, spaces=INDENT):
+    def walk(elem, depth):
+        if isinstance(elem, bool):
+            return str(elem).lower()
+        elif elem is None:
+            return 'null'
+        elif isinstance(elem, dict):
+            deep_indent_size = depth + spaces
+            deep_indent = ' ' * deep_indent_size
+            current_indent = ' ' * depth
+            lines = []
+            for k, v in elem.items():
+                lines.append(f'{deep_indent}{k}: {walk(v, deep_indent_size)}')
+            result = itertools.chain("{", lines, [current_indent + "}"])
+            return '\n'.join(result)
+        return str(elem)
+    return walk(current_value, INDENT)
--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,44 +1,25 @@
 from gendiff.parser import get_data
 
 
-def to_string(dct):
-    result = '{\n'
-    for k, v in dct.items():
-        if v[0] == ' ':
-            result += f' + {k}: {v[1]}\n'
-        elif v[1] == ' ':
-            result += f' - {k}: {v[0]}\n'
-        elif v[0] == v[1]:
-            result += f'   {k}: {v[0]}\n'
-        else:
-            result += f' - {k}: {v[0]}\n'
-            result += f' + {k}: {v[1]}\n'
-    return result + '}'
-
-
-def to_plain(dct):
-    for v in dct.values():
-        if isinstance(v[0], bool):
-            v[0] = str(v[0]).lower()
-        elif isinstance(v[1], bool):
-            v[1] = str(v[1]).lower()
-        elif v[0] is None:
-            v[0] = 'null'
-        elif v[1] is None:
-            v[1] = 'null'
-    return dct
-
-
 def generate_diff(file1_path, file2_path):
     dict1 = get_data(file1_path)
     dict2 = get_data(file2_path)
-    dict3 = {}
-    for k in sorted(dict1.keys() | dict2.keys()):
-        if k in dict1 and k in dict2:
-            dict3[k] = [dict1[k], dict2[k]]
-        elif k in dict1 and k not in dict2:
-            dict3[k] = [dict1[k], ' ']
-        else:
-            dict3[k] = [' ', dict2[k]]
-    result = to_plain(dict3)
-    return to_string(result)
+    diff = make_diff_tree(dict1, dict2)
+    return diff
+
+
+def make_diff_tree(dict1, dict2):
+    diff = []
+    for key in sorted(dict1.keys() | dict2.keys()):
+        if key not in dict2.keys():
+            diff.append({'status': 'deleted', 'key': key, 'val': dict1[key]})
+        elif key not in dict1.keys():
+            diff.append({'status': 'added', 'key': key, 'val': dict2[key]})
+        elif isinstance(dict1[key], dict) and isinstance(dict2[key], dict):
+            diff.append({'status': 'nested', 'key': key, 'children': make_diff_tree(dict1[key], dict2[key])})
+        elif dict1[key] == dict2[key]:
+            diff.append({'status': 'unchanged', 'key': key, 'val': dict1[key]})
+        elif dict1[key] != dict2[key]:
+            diff.append({'status': 'deleted', 'key': key, 'val': dict1[key]})
+            diff.append({'status': 'added', 'key': key, 'val': dict2[key]})
+    return diff
