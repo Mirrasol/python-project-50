0b59f80dbd038a331f62eb61184aec4d828e8d28
Elena Dobruk <elena.dobruk@yandex.ru>
2024-02-13 15:17:06 +0300

    fix complexity issue in plain.py

--- gendiff/formats/plain.py
+++ gendiff/formats/plain.py
@@ -1,13 +1,11 @@
 def to_string(value):
-    if isinstance(value, bool):
-        return str(value).lower()
-    elif value is None:
-        return 'null'
+    if isinstance(value, str):
+        return f"'{value}'"
     elif isinstance(value, dict):
         return '[complex value]'
-    elif isinstance(value, str):
-        return f"'{value}'"
-    return f'{value}'
+    elif value is None:
+        return 'null'
+    return str(value).lower()
 
 
 def to_plain(diff, path=''):
