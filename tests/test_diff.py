d293682d3adee83c26f5fbc67b711a147b09f3b4
Elena Dobruk <elena.dobruk@yandex.ru>
2024-02-14 16:36:36 +0300

    refactor tests

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,38 +1,20 @@
+import pytest
 from gendiff.gendiff import generate_diff
 
 
-def test_generate_flat_json():
-    with open('./tests/fixtures/result_stylish_flat.txt', 'r') as file_json:
-        result = file_json.read().strip('\n')
-        assert generate_diff('./tests/fixtures/file1.json', './tests/fixtures/file2.json') == result
+def read_expected(expected_path):
+    with open(expected_path, 'r') as file:
+        result = file.read().strip('\n')
+        return result
 
 
-def test_generate_flat_yml():
-    with open('./tests/fixtures/result_stylish_flat.txt', 'r') as file_yml:
-        result = file_yml.read().strip('\n')
-        assert generate_diff('./tests/fixtures/file1.yml', './tests/fixtures/file2.yml') == result
-
-
-def test_generate_tree():
-    with open('./tests/fixtures/result_stylish_tree.txt', 'r') as file_tree:
-        result = file_tree.read().strip('\n')
-        tree1 = './tests/fixtures/tree1.json'
-        tree2 = './tests/fixtures/tree2.json'
-        assert generate_diff(tree1, tree2) == result
-
-
-def test_generate_plain():
-    with open('./tests/fixtures/result_plain.txt', 'r') as file_plain:
-        result = file_plain.read().strip('\n')
-        tree1 = './tests/fixtures/tree1.yml'
-        tree2 = './tests/fixtures/tree2.yml'
-        assert generate_diff(tree1, tree2, 'plain') == result
-
-
-def test_generate_json():
-    with open('./tests/fixtures/result_json.json', 'r') as file_json:
-        result = file_json.read().strip('\n')
-        tree1 = './tests/fixtures/tree1.json'
-        tree2 = './tests/fixtures/tree2.json'
-        subject = generate_diff(tree1, tree2, 'json')
-        assert subject == result
+@pytest.mark.parametrize('file1_path, file2_path, expected, formatter',
+                         [
+                             ('tests/fixtures/tree1.json', 'tests/fixtures/tree2.json', 'tests/fixtures/result_stylish.txt', 'stylish'),
+                             ('tests/fixtures/tree1.yml', 'tests/fixtures/tree2.yml', 'tests/fixtures/result_stylish.txt', 'stylish'),
+                             ('tests/fixtures/tree1.yml', 'tests/fixtures/tree2.yml', 'tests/fixtures/result_plain.txt', 'plain'),
+                             ('tests/fixtures/tree1.json', 'tests/fixtures/tree2.json', 'tests/fixtures/result_json.json', 'json'),
+                         ])
+def test_generate_diff(file1_path, file2_path, expected, formatter):
+    result = read_expected(expected)
+    assert generate_diff(file1_path, file2_path, formatter) == result
